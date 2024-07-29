d037a3f57fbb0e04f30e033f0c1be5682da2563f
ThushanthaSanju <thushanthasanju8@gmail.com>
2024-07-29 09:27:30 +0530

    chore: Remove unnecessary code for storing user name in local storage

--- frontend/src/services/LoginService.ts
+++ frontend/src/services/LoginService.ts
@@ -9,10 +9,6 @@ export const login = async (data: LoginData) => {
   try {
     const response = await axiosInstance.post("/auth/login", data);
     localStorage.setItem("user", JSON.stringify(response));
-    localStorage.setItem(
-      "userName",
-      JSON.stringify(response.data.data.user.name)
-    );
 
     return response;
   } catch (error) {
